639020ab4af746de861e89a855b05157ad0cbf33
aastha058 <aasthapriya185@gmail.com>
2026-01-07 22:02:09 +0530

    Update user and product migration files; add foreign key constraint to tenant_id and define user_id relationship

--- database/migrations/2026_01_07_110924_create_products_table.php
+++ database/migrations/2026_01_07_110924_create_products_table.php
@@ -12,9 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+    $table->id();
+    $table->foreignId("user_id")->constrained()->onDelete("CASCADE");
+    $table->string('name');
+    $table->decimal('price', 10, 2);
+    $table->integer('quantity');
+    $table->timestamps();
+});
     }
 
     /**
