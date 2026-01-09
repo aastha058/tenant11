495e4aaa3225874dcfbd5c5669ad30722fbe9502
aastha058 <aasthapriya185@gmail.com>
2026-01-09 11:53:36 +0530

    Remove foreign key constraint for user_id in products table migration

--- database/migrations/2026_01_07_110924_create_products_table.php
+++ database/migrations/2026_01_07_110924_create_products_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
     $table->id();
-     $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('SET NULL');
     $table->string('name');
     $table->decimal('price', 10, 2);
     $table->integer('quantity');
